b06e37259c0f9abf75f28c486d38e31217a053ee
YatchenkoArD <yatchenkoard@gmail.com>
2026-03-09 21:25:25 +0300

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,6 +4,6 @@ from django.urls import reverse
 @pytest.mark.django_db
 def test_homepage_status(client):
     """Простой дымовой тест главной страницы"""
-    url = reverse('home')  # замени на реальный name из urls.py
+    url = reverse('Registration') 
     response = client.get(url)
     assert response.status_code == 200
